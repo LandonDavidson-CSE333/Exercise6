a946246029619f6fcccf220ced29b2041a6e4e5c
Landon Davidson <accounts@landond.tech>
2025-01-18 11:51:36 -0800

    Add: loop to print characters from end to start one by one

--- ex6.c
+++ ex6.c
@@ -12,19 +12,32 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
   // Open file specified by first argument
-  FILE* file = fopen(argv[1], "rb");
+  FILE *input = fopen(argv[1], "rb");
 
   // If fopen failed print an error and exit with failure code
-  if (file == NULL) {
+  if (input == NULL) {
     fprintf(stderr, "The file %s ", argv[1]);
     perror("failed to open for read");
     return EXIT_FAILURE;
   }
 
-
+  // Get size of the file
+  fseek(input, 0, SEEK_END);
+  long size = ftell(input);
+  // Iterate from end to start and printf each byte
+  for (; size > 0; size--) {
+    // Initialize cur_char as output parameter for fread
+    char cur_char;
+    // Moves cursor from end to start of file one at a time
+    fseek(input, size - 1, SEEK_SET);
+    // Read current byte in file and store in cur_char
+    fread(&cur_char, sizeof(char), 1, input);
+    // Print current byte stored in cur_char
+    printf("%c", cur_char);
+  }
 
   // Close file and prepare for successful exit
-  fclose(file);
+  fclose(input);
   return EXIT_SUCCESS;
 }
 
