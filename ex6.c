7d23c7783ec2cbeb4dc9f76f79d333d80f1fd946
Landon Davidson <accounts@landond.tech>
2025-01-18 09:22:12 -0800

    Add: fopen error checking for input

--- ex6.c
+++ ex6.c
@@ -8,11 +8,20 @@
 int main(int argc, char** argv) {
   // Check there is only one argument, exit with a failure code if not
   if (argc != 2) {
-    printf("You entered %d arguments, ex6 only takes 1", argc - 1);
+    fprintf(stderr, "You entered %d arguments, ex6 only takes 1\n", argc - 1);
     return EXIT_FAILURE;
   }
   // Open file specified by first argument
   FILE* file = fopen(argv[1], "rb");
+
+  // If fopen failed print an error and exit with failure code
+  if (file == NULL) {
+    fprintf(stderr, "The file %s ", argv[1]);
+    perror("failed to open for read");
+    return EXIT_FAILURE;
+  }
+
+
 
   // Close file and prepare for successful exit
   fclose(file);
